07f60ade17747d7a465d475b536906e99dd97491
bwkleingol <bartkleingoldewijk@hotmail.com>
2024-01-08 20:18:26 +0100

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			logo: {
+				src: './src/assets/app icon.svg',
+			  },
+			title: 'Knowledge Base',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Introduction',
+					link: '/introduction',
+				},
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
---
+++ src/content/docs/guides/example.md
@@ -0,0 +1,17 @@
+---
+title: Login, Users and Rights
+description: Login to your environment
+---
+
+## Login to your environment
+To login, you can visit traveltruster.com and select the login button for desktop or the profile icon on mobile phone.
+From there you can follow the prompt, and you'll be led to your account. 
+
+
+
+## Further reading
+
+- Read [about how-to guides](https://diataxis.fr/how-to-guides/) in the Diátaxis framework
+
+
+
