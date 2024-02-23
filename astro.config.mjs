0b355eb2cf9209c2d3a410764f3ccb366ba9c6bf
bwkleingol <44472920+bwkleingol@users.noreply.github.com>
2024-02-23 17:03:44 +0800

    Implemented Quickstart guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,76 +1,63 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import tailwind from "@astrojs/tailwind";
+
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
     favicon: './favicon.svg',
-    head:[
-      {
-        tag: 'link',
-        attrs:{
-          rel:'apple-touch-icon',
-          href:'./favicon.ico',
-          sizes:'32x32',
-        },
-      },
-    ],
-    logo: 
-    {
-      light:'./src/assets/DiversDesk_Logo_Day.svg',
-      dark: './src/assets/DiversDesk_Logo_Night.svg',
+    head: [{
+      tag: 'link',
+      attrs: {
+        rel: 'apple-touch-icon',
+        href: './favicon.ico',
+        sizes: '32x32'
+      }
+    }],
+    logo: {
+      light: './src/assets/DiversDesk_Logo_Day.svg',
+      dark: './src/assets/DiversDesk_Logo_Night.svg'
     },
     title: '',
     // social: {
     //   github: 'https://github.com/withastro/starlight'
     // },
-    sidebar: [
-    {
+    sidebar: [{
       label: 'Introduction',
       link: '/introduction'
-    }, 
-    // {
-    //   label: 'Quick Start Guide',
-    //   link: '/quickstart_guide'
-    // }, 
-    {
+    }, {
+      label: 'Quickstart Guide',
+      link: '/quickstart_guide'
+    }, {
       label: 'User Manual',
-      items: 
-      [
-        // Each item here is one entry in the navigation menu.
-        {
-          label: 'Login to your account',
-          link: '/user_manual/login/'
-        },
-        {
-          label: 'Menu and navigation',
-          link: '/user_manual/menu_and_navigation/'
-        },
-        {
-          label: 'Users and rights',
-          link: '/user_manual/users_and_rights/'
-        },
-        {
-          label: 'Booking and scheduling',
-          link: '/user_manual/booking_and_scheduling/'
-        },
-        {
-          label: 'Customer onboarding methods',
-          link: '/user_manual/customer_onboarding/'
-        },
-        {
-          label: 'Customer base',
-          link: '/user_manual/customer_base/'
-        }
-      ]
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Login to your account',
+        link: '/user_manual/login/'
+      }, {
+        label: 'Menu and navigation',
+        link: '/user_manual/menu_and_navigation/'
+      }, {
+        label: 'Users and rights',
+        link: '/user_manual/users_and_rights/'
+      }, {
+        label: 'Booking and scheduling',
+        link: '/user_manual/booking_and_scheduling/'
+      }, {
+        label: 'Customer onboarding methods',
+        link: '/user_manual/customer_onboarding/'
+      }, {
+        label: 'Customer base',
+        link: '/user_manual/customer_base/'
+      }]
     }, {
       label: 'Articles',
-      autogenerate: 
-      {
+      autogenerate: {
         directory: 'articles'
       }
     }]
-  }), tailwind()]
+  }), tailwind(), markdoc()]
 });
--- src/content/docs/quickstart_guide.md
+++ src/content/docs/quickstart_guide.md
@@ -3,13 +3,40 @@ title: Quickstart Guide
 description: Get started through a few simple settings
 ---
 
-import { LinkCard, CardGrid } from '@astrojs/starlight/components';
+Hi there! <br>
+Thank you for making it to our quickstart guide! We're delighted to have you here. 
+Please follow the 5 steps below and right away get started with our software.
+
+> "Well begun is half done." - Aristotle
+
+### 1. Create an account 
+Navigate to [traveltruster.com](https://www.traveltruster.com/) and create an account through the 'Sign up' hyperlink in the right top corner.
+![Sign up on traveltruster](/images/tt_landing_page_sign_up.png)
 
 
-We're delighted to have you here and want to invite you to get started with our software.
+### 2. Claim your business domain 
+Please [get in touch](mailto:info@diversdesk.com?subject=Request%20to%20create%20my%20Diversdesk%20page) to let us know you have created an account and are ready to get started using our software. We will in turn create your company's page and connect this to your account.
 
-To get started it is easy to get a few settings right. 
+:::tip[White lable]
+Did you know that we support white labeling? This allows you to enjoy:<br> 
+**-** A personalized URL with your company name embedded <br>
+**-** Your company logo displayed across each page of our platform.<br>
+This creates a personal and professional look towards your customers.
+:::
 
-1. 1: Create an account on traveltruster.com
-2. 1:  Enter your activities
+### 3. Personalize your company page and enter activities 
+After establishing your account, you can begin personalizing your own company page and enter your activities. These activities can then be quickly selected throughout our management software.
 
+Info on how to set up your personal page can be [found here](./user_manual/setup_your_personal_page) 
+
+### 4. Customize and connect the appropriate forms
+Using the Forms and Product pages you can customize and connect the desired registration and liability forms for each created product. [Read here](./articles/custom_registration_form) how to customize forms and connect the appropriate forms to each of your products. <br>
+This guarantees a smooth onboarding for each one of your customers.
+![Sign up on traveltruster](/images/tt_hamburger_dropdown_forms_page.png)
+
+### 5. Enter your staff members 
+Enter your staff members and allocate appropriate rights. How to do this can be read on the [Users and rights](./user_manual/users_and_rights) page within the user manual.
+
+#### You are now ready to start enjoying our software. 
+Please know we are here to assist you with any questions, unclarities or a simply a good chat :). <br>
+Don't hesitate to reach out!
