3c9f13a7f10e0b7f18519767ccffa15416e7c5c7
bwkleingol <44472920+bwkleingol@users.noreply.github.com>
2024-06-12 00:42:45 +0800

    Content updates

--- diversdesk_wip/src/utils/navigation.ts
+++ diversdesk_wip/src/utils/navigation.ts
@@ -2,27 +2,27 @@
 const navBarLinks = [
   { name: "Home", url: "/" },
   { name: "About", url: "/products" },
-  { name: "Pricing", url: "/services" },
-  { name: "Blog", url: "/blog" },
+  { name: "Pricing", url: "/pricing" },
+ // { name: "Blog", url: "/blog" },
   { name: "Contact", url: "/contact" },
 ];
 // An array of links for footer
 const footerLinks = [
   {
-    section: "Ecosystem",
+    section: "Support",
     links: [
       { name: "Documentation", url: "/welcome-to-docs/" },  
-      { name: "Tools & Equipment", url: "/products" },
-      { name: "Construction Services", url: "/services" },
+      { name: "FAQ", url: "/products" },
+      // { name: "Contact", url: "/services" },
     ],
   },
   {
     section: "Company",
     links: [
       { name: "About us", url: "#" },
-      { name: "Blog", url: "/blog" },
+      // { name: "Blog", url: "/blog" },
       { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
+      { name: "Contact", url: "#" },
     ],
   },
 ];
