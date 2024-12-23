341b3c7177a7253d50894b1e598d814bd4d089ab
bwkleingol <44472920+bwkleingol@users.noreply.github.com>
2024-12-23 12:45:31 +0800

    tweaks for seo and favicons updated

--- diversdesk/astro.config.mjs
+++ diversdesk/astro.config.mjs
@@ -10,7 +10,7 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://diversdesk.com',
+  site: 'https://www.diversdesk.com',
   image: {
     domains: ['images.unsplash.com'], 
   },  
