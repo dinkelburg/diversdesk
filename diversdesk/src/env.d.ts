7a0e5a54ef5b1a54e0913cc0a4fb8dcea888512f
Ruben Dinkelman <ruben@dinkelworks.nl>
2026-08-15 23:27:23 +0200

    feat: add TanStack signup form embed

--- diversdesk/src/env.d.ts
+++ diversdesk/src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly TANSTACK_APP_ORIGIN?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
